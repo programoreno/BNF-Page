6f259bb704ebf9833d3f1b770f52fe1705ecc8ce
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-21 21:26:20 +0200

    comimt de prueba., bien?

---
+++ BNF-PAGE/src/app/pages/zombies/zombies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zombies',
+  standalone: true,
+  imports: [],
+  templateUrl: './zombies.component.html',
+  styleUrl: './zombies.component.css'
+})
+export class ZombiesComponent {
+
+}
