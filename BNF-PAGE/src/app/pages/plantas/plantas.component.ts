c1d06d419ea7ef511127e95138b92355ad1e0de0
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-21 21:42:48 +0200

    json prueba creado

--- BNF-PAGE/src/app/pages/plantas/plantas.component.ts
+++ BNF-PAGE/src/app/pages/plantas/plantas.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PresentacionComponent } from "../../component/presentacion/presentacion.component";
+import { SkinsComponent } from "../../component/skins/skins.component";
 
 @Component({
   selector: 'app-plantas',
   standalone: true,
-  imports: [PresentacionComponent],
+  imports: [PresentacionComponent, SkinsComponent],
   templateUrl: './plantas.component.html',
   styleUrl: './plantas.component.css'
 })
