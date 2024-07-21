3e1c01d303834f698e6a5a1488112a632fbd9873
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-21 22:56:27 +0200

    avanzado bastante

--- BNF-PAGE/src/app/component/skins/skins.component.ts
+++ BNF-PAGE/src/app/component/skins/skins.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { PlantasService } from '../../service/plantas.service';
+import { Personaje } from '../../entity/Personaje';
+import { Datos } from '../../entity/Datos';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-skins',
@@ -8,5 +12,35 @@ import { Component } from '@angular/core';
   styleUrl: './skins.component.css'
 })
 export class SkinsComponent {
+
+  constructor(private plantasService: PlantasService){}
+
+  plantas:Personaje [];
+  skins:Datos[];
+  
+  nom = 'peashoter';
+
+  ngOnInit(): void {
+    this.plantasService.getPlantas().subscribe(dato => {
+      this.plantas = dato;
+      this.skins = this.plantas.flatMap(planta => planta.skins);
+    });
+  }
+
+  abrir_info(titulo: string, rareza: string,obtencion: string,frase: string) {
+    Swal.fire({
+      html:
+      `<div id="dialogo">
+          <div>
+              <h2>${titulo}</h2>
+              <p id="${rareza}">${rareza}</p>
+              <p>${obtencion}</p>
+              <p>${frase}</p>
+          </div> 
+      </div>`,
+      showCloseButton: true,
+      showConfirmButton: false,
+  });
+}
 
 }
