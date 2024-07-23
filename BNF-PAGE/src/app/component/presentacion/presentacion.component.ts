40d05340c39edaf29394204b7ba149d9a3b5e039
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-23 20:56:02 +0200

    mazorca termianda

--- BNF-PAGE/src/app/component/presentacion/presentacion.component.ts
+++ BNF-PAGE/src/app/component/presentacion/presentacion.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PlantasService } from '../../service/plantas.service';
 import { Personaje } from '../../entity/Personaje';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-presentacion',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './presentacion.component.html',
   styleUrl: './presentacion.component.css'
 })
