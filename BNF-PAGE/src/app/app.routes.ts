879990cff2451ab76154e20ce8e260b8d7fab6c8
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-24 17:29:42 +0200

    arreglado lsa pagina

--- BNF-PAGE/src/app/app.routes.ts
+++ BNF-PAGE/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PlantasComponent } from './pages/plantas/plantas.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AlbunPlantasComponent } from './pages/albun-plantas/albun-plantas.component';
 
 export const routes: Routes = [
     {path : 'home', component:HomeComponent},
     {path : '', redirectTo:'home',pathMatch:'full'},
-    {path : 'plantas/:planta', component:PlantasComponent},
+    {path : 'plantas', component:PlantasComponent},
+    {path : 'plantas/:planta', component:AlbunPlantasComponent},
 ];
---
+++ BNF-PAGE/src/app/pages/albun-plantas/albun-plantas.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { PresentacionComponent } from '../../component/presentacion/presentacion.component';
+import { TabsComponent } from '../../component/tabs/tabs.component';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-albun-plantas',
+  standalone: true,
+  imports: [PresentacionComponent, TabsComponent],
+  templateUrl: './albun-plantas.component.html',
+  styleUrl: './albun-plantas.component.css'
+})
+export class AlbunPlantasComponent implements OnInit{
+
+  constructor(private route:ActivatedRoute){}
+
+  nom:string;
+
+  ngOnInit(): void {
+    this.nom = this.route.snapshot.params['planta'];
+  }
+
+}
