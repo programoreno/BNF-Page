e9a16734f90ba1f24f424ab961307c0d55dab182
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-24 18:11:40 +0200

    listado de las plantas terminado

--- BNF-PAGE/src/app/pages/plantas/plantas.component.ts
+++ BNF-PAGE/src/app/pages/plantas/plantas.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PlantasService } from '../../service/plantas.service';
+import { Personaje } from '../../entity/Personaje';
 
 @Component({
   selector: 'app-plantas',
@@ -8,6 +10,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './plantas.component.html',
   styleUrl: './plantas.component.css'
 })
-export class PlantasComponent{
+export class PlantasComponent implements OnInit{
 
+  plantas:Personaje[];
+
+  constructor(private plantasService: PlantasService){}
+
+  ngOnInit(): void {
+    this.plantasService.getPlantas().subscribe(dato => {
+      this.plantas = dato;
+    });
+  }
 }
