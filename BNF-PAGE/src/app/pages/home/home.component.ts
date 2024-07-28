6c88f40ec2257c6cd29d0dccfa21072be3f6ee2e
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-28 23:28:07 +0200

    HOME DE LOCOS

--- BNF-PAGE/src/app/pages/home/home.component.ts
+++ BNF-PAGE/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { InicioComponent } from "../../component/inicio/inicio.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, InicioComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
