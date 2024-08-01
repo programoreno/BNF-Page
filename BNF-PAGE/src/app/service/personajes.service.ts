fd69968a1621fa34f66d37f9524c93291bfeb183
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-08-01 18:36:22 +0200

    json problemas arreglados

--- BNF-PAGE/src/app/service/personajes.service.ts
+++ BNF-PAGE/src/app/service/personajes.service.ts
@@ -9,14 +9,23 @@ import { Observable, map } from 'rxjs';
 export class PersonajesService {
 
   constructor(private http:HttpClient) { }
-url:string;
 
-  getPersonajes(team:string):Observable<Personaje[]>{
-    console.log(team);
-    this.url='/assets/'+team+'.json';
-    console.log(this.url);
-    return this.http.get<{ personajes: Personaje[] }>('assets/plantas.json').pipe(
-      map(response => response.personajes)
+
+  getPersonajes(team: string): Observable<Personaje[]> {
+    return this.http.get(`/assets/${team}.json`).pipe(
+      map((data: any) => {
+        // Encuentra la propiedad que contiene el array de personajes
+        const keys = Object.keys(data);
+        for (const key of keys) {
+          if (Array.isArray(data[key])) {
+            return data[key] as Personaje[];
+          }
+        }
+        // Si no se encuentra una propiedad que sea un array, devuelve un array vacío
+        return [];
+      })
     );
   }
+  
+  
 }
