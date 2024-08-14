b78cb43662c41ccc1d438f18ad317e9a265d7156
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-08-14 19:48:04 +0200

    problema al cargar el albun perop los detalles de cada personaje funciona

--- BNF-PAGE/src/app/app.routes.ts
+++ BNF-PAGE/src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { AlbunPlantasComponent } from './pages/albun-plantas/albun-plantas.component';
 import { AlbunZombiesComponent } from './pages/albun-zombies/albun-zombies.component';
 import { AlbunComponent } from './pages/albun/albun.component';
+import { DetallesPersonajeComponent } from './pages/detalles-personaje/detalles-personaje.component';
 
 export const routes: Routes = [
     {path : 'home', component:HomeComponent},
     {path : '', redirectTo:'home',pathMatch:'full'},
     {path : ':pagina', component:AlbunComponent},
-    {path : 'plantas/:personaje', component:AlbunPlantasComponent},
-    {path : 'zombies/:personaje', component:AlbunZombiesComponent},
+    {path : ':pagina/:personaje', component:DetallesPersonajeComponent}
 ];
