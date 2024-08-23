14b73ef39039e12d26c25de610c87f917c90e952
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-08-23 13:00:40 +0200

    ha q cambiar todo lo de las paginas de planmtas y zombies ya que repercute a la de mapas y todo, MAPAS ENMPEZADO

--- BNF-PAGE/src/app/app.routes.ts
+++ BNF-PAGE/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AlbunComponent } from './pages/albun/albun.component';
 import { DetallesPersonajeComponent } from './pages/detalles-personaje/detalles-personaje.component';
+import { MapasComponent } from './pages/mapas/mapas.component';
 
 export const routes: Routes = [
     {path : 'home', component:HomeComponent},
     {path : '', redirectTo:'home',pathMatch:'full'},
-    {path : ':pagina', component:AlbunComponent},
-    {path : ':pagina/:personaje', component:DetallesPersonajeComponent}
+    /*{path : ':pagina', component:AlbunComponent},
+    {path : ':pagina/:personaje', component:DetallesPersonajeComponent},*/
+    {path : 'mapas', component:MapasComponent}
 ];
