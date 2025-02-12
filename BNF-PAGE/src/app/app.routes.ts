5dc6028a8ef315128b1cac9fc659ea257ba9253a
Guille <programoreno@gmail.com>
2025-02-12 18:26:32 +0100

    pagina emotes configurada

--- BNF-PAGE/src/app/app.routes.ts
+++ BNF-PAGE/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AlbunComponent } from './pages/albun/albun.component';
 import { DetallesPersonajeComponent } from './pages/detalles-personaje/detalles-personaje.component';
 import { MapasComponent } from './pages/mapas/mapas.component';
+import { EmotesComponent } from './pages/emotes/emotes.component';
 
 export const routes: Routes = [
     {path : 'home', component:HomeComponent},
@@ -11,5 +12,6 @@ export const routes: Routes = [
     {path : 'zombies', component:AlbunComponent},
     {path : 'general', component:AlbunComponent},
     {path : ':pagina/:personaje', component:DetallesPersonajeComponent},
-    {path : 'mapas', component:MapasComponent}
+    {path : 'mapas', component:MapasComponent},
+    {path : 'emotes', component:EmotesComponent}
 ];
