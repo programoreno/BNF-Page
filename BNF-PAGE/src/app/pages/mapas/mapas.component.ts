d5007b9af2e4bf675853462d0c5b772a78688f9a
guillermooom <programoreno@gmail.com>
2025-02-22 15:39:58 +0100

    Menu sin boostrap bien

--- BNF-PAGE/src/app/pages/mapas/mapas.component.ts
+++ BNF-PAGE/src/app/pages/mapas/mapas.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LugaresComponent } from "../../component/lugares/lugares.component";
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../../component/footer/footer.component";
+import { MenuComponent } from "../../component/menu/menu.component";
 
 @Component({
   selector: 'app-mapas',
   standalone: true,
-  imports: [CommonModule, LugaresComponent, FooterComponent],
+  imports: [CommonModule, LugaresComponent, FooterComponent, MenuComponent],
   templateUrl: './mapas.component.html',
   styleUrl: './mapas.component.css'
 })
