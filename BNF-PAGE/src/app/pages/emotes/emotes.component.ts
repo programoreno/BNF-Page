a11d71a39ccfd9c9070d0d78bdb185bf247881a2
guillermooom <programoreno@gmail.com>
2025-02-24 19:45:31 +0100

    menu en todas las apps

--- BNF-PAGE/src/app/pages/emotes/emotes.component.ts
+++ BNF-PAGE/src/app/pages/emotes/emotes.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../../component/footer/footer.component";
 import { GestosComponent } from "../../component/gestos/gestos.component";
+import { MenuComponent } from "../../component/menu/menu.component";
 
 @Component({
   selector: 'app-emotes',
   standalone: true,
-  imports: [CommonModule, FooterComponent, GestosComponent],
+  imports: [CommonModule, FooterComponent, GestosComponent, MenuComponent],
   templateUrl: './emotes.component.html',
   styleUrl: './emotes.component.css'
 })
